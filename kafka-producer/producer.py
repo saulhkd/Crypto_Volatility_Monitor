b62d0227e7a38b31f82bc46309ce252224b2f582
saulhkd <saulmarti2@hotmail.com>
2026-02-27 09:46:21 +0100

    Changes in the .py file and PySpark
    Trying things with Spark notebooks and Kafka

--- kafka-producer/producer.py
+++ kafka-producer/producer.py
@@ -7,7 +7,8 @@ import os
 # Configuración (Leyendo variables de entorno)
 KAFKA_BROKER = os.getenv('KAFKA_BROKER', 'kafka:29092')
 TOPIC = 'crypto_prices'
-API_URL = 'https://api.coincap.io/v2/assets'
+API_KEY = os.getenv('API_KEY')
+API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
 
 def create_producer():
     # Esperamos un poco a que Kafka arranque completamente
@@ -17,23 +18,37 @@ def create_producer():
                 bootstrap_servers=[KAFKA_BROKER],
                 value_serializer=lambda x: json.dumps(x).encode('utf-8')
             )
-            print("✅ Conectado a Kafka exitosamente")
+            print("Conectado a Kafka exitosamente")
             return producer
         except Exception as e:
-            print(f"⚠️ Esperando a Kafka... ({e})")
+            print(f"Esperando a Kafka... ({e})")
             time.sleep(5)
 
 def get_crypto_data():
+    parameters = {
+      'symbol':'BTC,ETH,SOL',
+      'convert':'USD'
+    }
     try:
-        response = requests.get(API_URL)
+        response = requests.get(API_URL, headers={'X-CMC_PRO_API_KEY': API_KEY}, params=parameters)
         if response.status_code == 200:
             data = response.json()['data']
-            # Filtramos solo Bitcoin y Ethereum para no saturar
-            target_coins = ['bitcoin', 'ethereum', 'solana']
-            return [coin for coin in data if coin['id'] in target_coins]
+            clean_data = []
+            # Usamos .values() para iterar sobre los valores del diccionario, que son los objetos de las criptomonedas
+            for coin_info in data.values():
+                clean_data.append({
+                    'id': coin_info['slug'],               # Ej: 'bitcoin'
+                    'symbol': coin_info['symbol'],         # Ej: 'BTC'
+                    'priceUsd': coin_info['quote']['USD']['price'], # Buscamos el precio anidado
+                    'last_updated': coin_info['quote']['USD']['last_updated']
+                })
+            
+            return clean_data
+            
         else:
-            print(f"Error API: {response.status_code}")
-            return []
+            # Añadimos response.text para ver exactamente por qué se queja la API si falla
+            print(f"Error API: {response.status_code} - {response.text}")
+            return []     
     except Exception as e:
         print(f"Error Request: {e}")
         return []
@@ -47,10 +62,10 @@ if __name__ == '__main__':
         for coin in cryptos:
             # Enviamos el mensaje a Kafka
             producer.send(TOPIC, value=coin)
-            print(f"🚀 Enviado: {coin['id']} - ${coin['priceUsd']}")
+            print(f"Enviado: {coin['id']} - ${coin['priceUsd']}")
         
         # Forzar el envío inmediato
         producer.flush()
         
-        # Esperar 10 segundos antes de la siguiente consulta
-        time.sleep(10)
+        # Esperar 30 segundos antes de la siguiente consulta
+        time.sleep(30)
